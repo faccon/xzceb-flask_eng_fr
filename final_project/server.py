1836bc04ba3910b2500f08d40642e8160bda7dd7
faccon <iamadeniyiismail@gmail.com>
2022-03-23 11:39:50 +0000

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,36 @@
+"""
+ Routing ***
+"""
+
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
+import machinetranslation
 
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    """ translates english text from url args to french
+        from /englishToFrench
+    """
+    text_to_translate = request.args.get('textToTranslate')
+    traslated_text = machinetranslation.translator.english_to_french(text_to_translate)
+    return traslated_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    """ translates french text from url args to english
+        from /frenchToEnglish route
+    """
+    text_to_translate = request.args.get('textToTranslate')
+    traslated_text = machinetranslation.translator.french_to_english(text_to_translate)
+    return traslated_text
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    """ renders the html paage to / route """
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
